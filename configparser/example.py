2a6f1184aaea2c32a2c37f0ac22af378b61fddfb
seokhyun <hsh2438@naver.com>
2020-07-07 20:06:10 +0900

    Create example.py

---
+++ configparser/example.py
@@ -0,0 +1,22 @@
+import configparser
+
+""" create config file """
+config = configparser.ConfigParser()
+
+config['database'] = {}
+config['database']['host'] = '127.0.0.1'
+config['database']['port'] = '3306'
+config['database']['user'] = 'user01'
+config['database']['passwd'] = 'passwd01'
+config['database']['db_name'] = 'test'
+config['database']['charset'] = 'utf8'
+
+with open('config.ini', 'w') as configfile:
+    config.write(configfile)
+
+
+""" read config file """
+config = configparser.ConfigParser()
+config.read('config.ini')
+ 
+print(int(config['database']['port']))
