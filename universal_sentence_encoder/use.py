517f4acef092edb619332d50d2127c44d81c7550
seokhyun <hsh2438@naver.com>
2020-07-15 20:29:41 +0900

    Update use.py

--- universal_sentence_encoder/use.py
+++ universal_sentence_encoder/use.py
@@ -1,6 +1,17 @@
 import tensorflow as tf
 import tensorflow_hub as hub
 
+
+gpu_num = 1
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        tf.config.experimental.set_visible_devices(gpus[gpu_num], 'GPU')
+        tf.config.experimental.set_memory_growth(gpus[gpu_num], True)
+    except RuntimeError as e:
+        print(e)
+        
 class UniversalSentenceEncoder:
     
     def __init__(self, model_dir):
